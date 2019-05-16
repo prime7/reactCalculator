25d8ca45d8bdf50362f75f76a7b3b6c0d4982dde
Tarek <tarek5701@gmail.com>
2019-05-16 14:19:48 +0600

    Deploy

---
+++ build/precache-manifest.b068621baa00f80e445113397346bb27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdae0adbfdc8eac06b142a583182d474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed79d390289e20c7fe78",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "72873117e1a3846ea768",
+    "url": "/static/css/main.30e0f50c.chunk.css"
+  },
+  {
+    "revision": "ed79d390289e20c7fe78",
+    "url": "/static/js/2.420b3865.chunk.js"
+  },
+  {
+    "revision": "72873117e1a3846ea768",
+    "url": "/static/js/main.8c8a0878.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
